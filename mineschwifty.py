93452beb328039302fe40d1d336a926781e693cb
titusz <tp@py7.de>
2016-11-09 17:46:00 +0100

    fix indent and code style :)

--- mineschwifty.py
+++ mineschwifty.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python
-import json, requests
+from __future__ import print_function
+import json
 import yaml
+import requests
 
 f = open('mineschwifty.yml')
 # use safe_load instead load
 config = yaml.safe_load(f)
 f.close()
 
-sortbuffer_val = 0;
-sortbuffer_key = '';
+sortbuffer_val = 0
+sortbuffer_key = ''
+
 
 def get_profit_index(key, coin):
-   "sort the coin by profit"
-   global sortbuffer_key,sortbuffer_val
-   #hack-adi-hack, das dollarsign muss weg :-)
-   profit_val= float(coin['profit'][1:])
-   if ( profit_val > sortbuffer_val):
-       sortbuffer_key = key
-       sortbuffer_val = profit_val
-   return (sortbuffer_key,sortbuffer_val)
+    """sort the coin by profit"""
+    global sortbuffer_key, sortbuffer_val
+    # hack-adi-hack, das dollarsign muss weg :-)
+    profit_val = float(coin['profit'][1:])
+    if profit_val > sortbuffer_val:
+        sortbuffer_key = key
+        sortbuffer_val = profit_val
+    return sortbuffer_key, sortbuffer_val
 
 
 for i in (config['coins']):
-    params=dict(
-        hr = config['coins'][i]['hashrate'],
-        p = config['coins'][i]['powerwall'],
-        cost = config['powercost']
+    params = dict(
+        hr=config['coins'][i]['hashrate'],
+        p=config['coins'][i]['powerwall'],
+        cost=config['powercost']
     )
     resp = requests.get(url=config.get('url')+str(config['coins'][i]['index'])+'.json', params=params)
     data = json.loads(resp.text)
-    get_profit_index(i,data)
+    get_profit_index(i, data)
 
-print sortbuffer_key,sortbuffer_val
 
-print config['coins'][sortbuffer_key]['command_activate']
+print(sortbuffer_key, sortbuffer_val)
+print(config['coins'][sortbuffer_key]['command_activate'])
 #exec(config['coins'][sortbuffer_key]['command_activate'])
