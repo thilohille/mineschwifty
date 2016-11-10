3fbe80f61e1a99a8cbd598a52288a2dd777bdd74
Thilo Hille <hille@equinoxe.de>
2016-11-10 14:38:08 +0100

    tweaked on statehandling

--- mineschwifty.py
+++ mineschwifty.py
@@ -7,22 +7,22 @@ f = open('mineschwifty.yml')
 config = yaml.safe_load(f)
 
 # state file. contains the key of the current coin
-try:
-    s = open(config['statefilename'], 'rw+')
-    state = s.readline()
-except (IOError, ValueError):
-    s = open(config['statefilename'], 'w+')
-    state = 'nothing'
+s = open(config['statefilename'], 'a+')
+s.seek(0)
+current_state = s.read()
+s.close()
+if current_state == '':
+    current_state = 'nothing'
 
 sortbuffer_val = 0
 sortbuffer_key = ''
 
 def escalate(message):
     #todo: bullet message? email?
-    print(message)
+    print('ESC: %s'%(message))
 
 def control_miner(coin, command):
-    print(config['coins'][coin][command])
+    print('EXEC: %s'%(config['coins'][coin][command]))
     #exec(config['coins'][coin][command]);
 
 def get_profit_index(key, coin):
@@ -42,15 +42,18 @@ for i in (config['coins']):
         p=config['coins'][i]['powerwall'],
         cost=config['powercost']
     )
-    resp = requests.get(url=config.get('url')+str(config['coins'][i]['index'])+'.json', params=params)
-    get_profit_index(i, resp.json())
+    try:
+        resp = requests.get(url=config.get('url')+str(config['coins'][i]['index'])+'.json', params=params)
+        get_profit_index(i, resp.json())
+    except:
+        escalate('warning: failed to fetch wtm-data for %s.'%(i))
 
 
-if sortbuffer_key != state:
+if sortbuffer_key != current_state:
     print(sortbuffer_key, sortbuffer_val)
     #if we know the current miner, we stop it
-    if state in config['coins']:
-        control_miner(state,'command_stop')
+    if current_state in config['coins']:
+        control_miner(current_state,'command_stop')
     else:
         #try to stop all possible miners..
         for c in config['coins']:
@@ -58,12 +61,14 @@ if sortbuffer_key != state:
 
     #then we start the new miner if we have a profitable currency
     if sortbuffer_val > 0:
-        escalate('mining: switched from %s to %s at aprox. $%0.2f'%(state,sortbuffer_key,sortbuffer_val));
+        escalate('mining: switched from %s to %s at aprox. $%0.2f'%(current_state,sortbuffer_key,sortbuffer_val));
         control_miner(sortbuffer_key,'command_start')
-        state = sortbuffer_key
+        current_state = sortbuffer_key
     else:
         escalate('not mining: no profitable currency found');
-        state = 'nothing'
-    s.write(state)
+        current_state = 'nothing'
+    s = open(config['statefilename'], 'w+')
+    s.write(current_state)
+    s.close()
 f.close()
 
